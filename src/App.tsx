ca229b34316ab18fc3778ce35128b6398b973251
Anton J Olsson <anton.j.olsson@bredband.net>
2023-07-10 11:01:14 +0200

    feat: 🎸 draw symbols when clicking

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import {Header} from "./components/Header";
 import {Board} from "./components/Board";
 import './App.css'
 import {MessageArea} from "./components/MessageArea";
+import {Game} from "./Game";
 
 function App() {
     const [message, setMessage] = useState('Player one\'s turn...')
+    const game = new Game()
   return (
     <div className="app">
         <Header/>
         <main>
-            <Board />
+            <Board game={game}/>
             <MessageArea message={message}/>
         </main>
 
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,12 +1,35 @@
 import './Board.css'
-import {useEffect, useRef} from "react";
+import '../App.css'
+import React, {useEffect, useRef, useState} from "react";
+import {Game, ISquare, Player} from "../Game";
 
-const GRID_SIZE = 3
+function Square(props: { logicSquare: ISquare, game: Game, i: number, playerToSymbolMap: Map<Player, string> }) {
+    const [symbol, setSymbol] = useState('')
 
-export function Board() {
+    function onClick(): void {
+        if (!props.logicSquare.player) {
+            const player = props.game.currentPlayer
+            setSymbol(props.playerToSymbolMap.get(player)!)
+            props.game.setSquare(props.i)
+        }
+    }
+
+    return <div className={`square ${!symbol ? " available" : ""}`} onMouseUp={onClick}>
+        {symbol && props.logicSquare.player
+            ? <span className={`material-symbols-outlined ${props.logicSquare.player}`}>{symbol}</span>
+            : ''}
+    </div>;
+}
+
+export function Board(props: {game: Game}) {
     const canvasRef =  useRef<HTMLCanvasElement | null>(null)
+    const gridSize = props.game.gridSize
+    const playerToSymbolMap = new Map<Player, string>([
+        [Player.PLAYER_ONE, 'close'],
+        [Player.PLAYER_TWO, 'circle']
+    ])
 
-    function initGame(canvas: HTMLCanvasElement) {
+    /*function drawBoard(canvas: HTMLCanvasElement) {
         const ctx = canvas.getContext('2d')!
         const gridHeight = canvasRef.current!.height
         const gridWidth = canvasRef.current!.width
@@ -14,30 +37,41 @@ export function Board() {
         ctx.strokeStyle = '#333'
         ctx.lineWidth = 3
         ctx.beginPath()
-        for (let i = 1; i < GRID_SIZE; i++) {
-            ctx.moveTo(0, gridHeight * i / GRID_SIZE)
-            ctx.lineTo(gridWidth, gridHeight * i / GRID_SIZE)
-            console.log([0, gridHeight * i / GRID_SIZE], [gridHeight, gridHeight * i / GRID_SIZE])
+        for (let i = 1; i < gridSize; i++) {
+            ctx.moveTo(0, gridHeight * i / gridSize)
+            ctx.lineTo(gridWidth, gridHeight * i / gridSize)
+            console.log([0, gridHeight * i / gridSize], [gridHeight, gridHeight * i / gridSize])
         }
         ctx.stroke()
         ctx.lineWidth = 7
         ctx.beginPath()
-        for (let i = 1; i < GRID_SIZE; i++) {
-            ctx.moveTo(i / GRID_SIZE * gridWidth, 0)
-            ctx.lineTo(i / GRID_SIZE * gridWidth, gridHeight)
-            console.log([i / GRID_SIZE * gridHeight, 0], [i / GRID_SIZE * gridHeight, gridHeight])
+        for (let i = 1; i < gridSize; i++) {
+            ctx.moveTo(i / gridSize * gridWidth, 0)
+            ctx.lineTo(i / gridSize * gridWidth, gridHeight)
+            console.log([i / gridSize * gridHeight, 0], [i / gridSize * gridHeight, gridHeight])
         }
         ctx.stroke()
 
-    }
+    }*/
 
     useEffect(() => {
         if (canvasRef.current) {
-            initGame(canvasRef.current)
+            // drawBoard(canvasRef.current)
         }
     }, [])
 
-    return <canvas ref={canvasRef}>
+    /*return <canvas ref={canvasRef}}>
 
-    </canvas>;
+    </canvas>;*/
+
+    function getGridAxisLayout(): string {
+        return Array(gridSize).fill('1fr').join(' ');
+    }
+
+    return <div id={'board'}>
+        <div id={'board-grid-bg'} style={{gridTemplateRows: getGridAxisLayout(), gridTemplateColumns: getGridAxisLayout()}}>
+            {Array(gridSize * gridSize).fill('').map((_, i) =>
+                <Square key={i} playerToSymbolMap={playerToSymbolMap} game={props.game} logicSquare={props.game.board[i]} i={i}/>)}
+        </div>
+    </div>
 }
