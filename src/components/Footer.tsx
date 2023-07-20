4dd93190c4b6a4025228057350ecad5fbdbce503
Anton J Olsson <anton.j.olsson@bredband.net>
2023-07-20 16:46:47 +0200

    feat: 🎸 github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,5 +2,7 @@ import React, {ReactElement} from "react";
 import './Footer.css'
 
 export function Footer(): ReactElement {
-    return <div id={'footer'}></div>;
+    return <div id={'footer'}>
+        <a id={'source-link'} href={'https://github.com/antonjolsson/tic-tac-toe'}>Source on GitHub</a>
+    </div>;
 }
