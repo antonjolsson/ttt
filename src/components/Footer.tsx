61b1872a33e9f92516df5950db00015ce189de70
Anton J Olsson <anton.j.olsson@bredband.net>
2023-07-21 18:35:22 +0200

    fix: 🐛 firefox background banding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,6 @@ import './Footer.css'
 
 export function Footer(): ReactElement {
     return <div id={'footer'}>
-        <a id={'source-link'} href={'https://github.com/antonjolsson/tic-tac-toe'}>Source on GitHub</a>
+        <a id={'source-link'} href={'https://github.com/antonjolsson/ttt'}>Source on GitHub</a>
     </div>;
 }
